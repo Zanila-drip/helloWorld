687e4cf4cc6f72f7d1481abad237becdfc2e0812
Zanila-drip <mleo0499@gmail.com>
2026-02-14 11:29:14 -0600

    nuevos println

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ fn main() {
     //Si hago rusct a main.rs se hace un ejecutable
     //y haciendo ./main.rs lo ejecuto en terminal!
     println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    
     // //
 
 
